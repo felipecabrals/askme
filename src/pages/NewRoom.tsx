413321bb385c60b721f182b250b36d98484b29de
Felipe Dias <flp.dsc@gmail.com>
2021-09-06 19:28:35 -0300

    criando sala de Q&A

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,27 +1,54 @@
-import { Link } from 'react-router-dom';
+import { FormEvent } from 'react'
+import { Link, useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 import '../styles/auth.scss';
 import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
+import { database } from '../services/firebase';
 
 export function NewRoom() {
   const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&A ao vivo</strong>
         <p>Tire suas dúvidas da sua audiencia em tempo real</p>
-      </aside> 
+      </aside>
       <main className="main-content">
         <div>
           <img src={logoImg} alt="Askme" />
-          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input
+              type="text"
+              placeholder="Nome da da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
+            />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
